9a83e2a6eb160f0f5471e8a00482f6f07cfccf59
Denis <156504836+Denis-Kotlyarov@users.noreply.github.com>
2024-02-14 13:16:48 +0000

    какой-то там мусташ, вроде как данные парсим, я вообще в ауте

---
+++ src/jobs.js
@@ -0,0 +1,18 @@
+const JOBS = [
+    {
+        id: 1,
+        title: 'Fronted developer',
+        location: 'Benglaur',
+        salary: '12000',
+        posted: '20.20.2020'
+    },
+    {
+        id: 1,
+        title: 'Fronted developer',
+        location: 'Benglaur',
+        salary: '12000',
+        posted: '20.20.2020'
+    }
+]
+
+module.exports = JOBS
